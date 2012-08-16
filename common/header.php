277522cb2b785d665f743c4d727a199399d8082c
Kimon Keramidas <kimonkeramidas@kimon-keramidass-imac.local>
2012-08-16 11:42:21 -0400

    Initial Commit

---
+++ common/header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php echo get_html_lang(); ?>">
+<head>
+<title><?php echo settings('site_title'); echo isset($title) ? ' | ' . $title : ''; ?></title>
+
+<!-- Meta -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="<?php echo settings('description'); ?>" />
+
+<?php echo auto_discovery_link_tag(); ?>
+
+<!-- Plugin Stuff -->
+<?php plugin_header(); ?>
+
+<!-- Stylesheets -->
+<?php
+queue_css(array('app', 'foundation'));
+display_css();
+?>
+
+<!-- JavaScripts -->
+<?php
+queue_js(array('app', 'foundation'));
+display_js();
+?>
+
+
+<!-- Start Lightbox includes -->
+<script type="text/javascript" src="<?php echo WEB_ROOT;?>/themes/gerould-tribute/common/lightbox/js/prototype.js"></script>
+<script type="text/javascript" src="<?php echo WEB_ROOT;?>/themes/gerould-tribute/common/lightbox/js/scriptaculous.js?load=effects,builder"></script>
+<script type="text/javascript" src="<?php echo WEB_ROOT;?>/themes/gerould-tribute/common/lightbox/js/lightbox.js"></script>
+<!-- End Lightbox includes -->
+
+</head>
+
+<body<?php echo isset($bodyid) ? ' id="'.$bodyid.'"' : ''; ?><?php echo isset($bodyclass) ? ' class="'.$bodyclass.'"' : ''; ?>>
+<?php plugin_body(); ?>
+<div class="container">
+	<?php plugin_page_header(); ?>
+	<div class="row">
+		<div class="twelve columns">
+			<h1 id="site-title">
+				<?php echo link_to_home_page(custom_display_logo()); ?>
+			</h1>
+			<div id="primary-nav">
+				<ul class="nav-bar">
+					<li>
+						<?php echo nav (
+									array(
+									'Browse Items' => uri('items'),
+									'Browse Collections' => uri('collections')
+								)
+							);
+						?>
+					</li>
+					<li>
+						<?php echo link_to_advanced_search('More Search Options'); ?>
+					</li>
+					<li id="search-container">
+						<?php echo simple_search(); ?>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
